5e493b32b98ceb67a0a76fdc7e29310cee2e5446
RukiyeBali <145977861+RukiyeBali@users.noreply.github.com>
2023-09-25 14:24:46 +0300

    Add files via upload

---
+++ manav-uygulaması/manav-uygulaması/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace manav_uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstSepet = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSeçereksil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // lstSepet
+            // 
+            this.lstSepet.FormattingEnabled = true;
+            this.lstSepet.Location = new System.Drawing.Point(285, 12);
+            this.lstSepet.Name = "lstSepet";
+            this.lstSepet.Size = new System.Drawing.Size(192, 134);
+            this.lstSepet.TabIndex = 1;
+            this.lstSepet.SelectedIndexChanged += new System.EventHandler(this.lstSepet_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.SystemColors.Menu;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnEkle.Location = new System.Drawing.Point(25, 121);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(109, 25);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Sepete Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnTemizle.Location = new System.Drawing.Point(74, 165);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(109, 25);
+            this.btnTemizle.TabIndex = 4;
+            this.btnTemizle.Text = "Sepeti Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(41, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ürün";
+            // 
+            // btnSeçereksil
+            // 
+            this.btnSeçereksil.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSeçereksil.Location = new System.Drawing.Point(140, 123);
+            this.btnSeçereksil.Name = "btnSeçereksil";
+            this.btnSeçereksil.Size = new System.Drawing.Size(118, 23);
+            this.btnSeçereksil.TabIndex = 6;
+            this.btnSeçereksil.Text = "Seçerek Sil";
+            this.btnSeçereksil.UseVisualStyleBackColor = true;
+            this.btnSeçereksil.Click += new System.EventHandler(this.btnSeçereksil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(526, 279);
+            this.Controls.Add(this.btnSeçereksil);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstSepet);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Manav";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstSepet;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSeçereksil;
+    }
+}
+
